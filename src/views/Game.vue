57e3c961a877402935dc4476a17d44751e0fb1a8
Jinjos <tommer.shahar@dragontailsystems.com>
2019-01-08 10:36:56 +0200

    add gui board to select

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -2,30 +2,15 @@
   <div class="game">
     <div class="actions flex-center">
       <button :class="{ boardSet: boardSet}" @click="setBoard" :disabled="boardSet">{{boardStatus}}</button>
-      <button :class="{ activeShips: boardSet}" @click="setShipOnBoard" :disabled="!boardSet">Set a New Ship</button>
+      <!-- <button :class="{ activeShips: boardSet}" @click="setShipOnBoard" :disabled="!boardSet">Set a New Ship</button> -->
     </div>
-    <div class="setShip">
-     <p class="ship-length">
+    <div class="shipConfig flex-center">
+     <div class="ship-length">
       <label for="shipLength">
         Ship Length:
       </label>
        <input v-model="shipLength" id="shipLength" placeholder="enter ship length">
-    </p>
-    <p>
-      Ship Coordinate:
-      Latitude:
-      <ul>
-        <li v-for="(cell,index) in boardSize" :key="cell" :class="selectedCoordinate(index,'latitude')" @click="setCoordinate(index,'latitude')">
-        {{ index }}
-        </li>
-      </ul>
-      Longtitude:
-       <ul>
-        <li v-for="(cell,index) in boardSize" :key="cell" :class="selectedCoordinate(index,'longtitude')" @click="setCoordinate(index,'longtitude')">
-        {{ index }}
-        </li>
-      </ul>
-    </p>
+    </div>
     <div class="ship-dir">
        <div>Ship Direction:</div>
        <div class="ship-dir-selection">
@@ -37,10 +22,39 @@
            <input type="radio" id="vertical" value="1" v-model="shipDirection">
           <label for="vertical">Vertical</label>
          </div>
-
        </div>
-
     </div>
+    </div>
+    <div class="board">
+      <table>
+        <tr v-for="(row,x) in boardSize" :key="row">
+          <td
+            v-for="(cell,y) in boardSize"
+            :key="cell"
+            :x="x"
+            :y="y"
+            @click.prevent="setShipOnBoard($event)"
+            :class="{ activeShips: boardSet}"></td>
+        </tr>
+      </table>
+
+      <!-- Ship Coordinate:
+      Latitude:
+      <ul>
+        <li v-for="(cell,index) in boardSize" :key="cell" :class="selectedCoordinate(index,'latitude')" @click="setCoordinate(index,'latitude')">
+        {{ index }}
+        </li>
+      </ul>
+      Longtitude:
+       <ul>
+        <li v-for="(cell,index) in boardSize" :key="cell" :class="selectedCoordinate(index,'longtitude')" @click="setCoordinate(index,'longtitude')">
+        {{ index }}
+        </li>
+      </ul> -->
+    </div>
+    <div
+      class="error-message flex-center"
+      v-if="errorMessage">{{errorMessage}}</div>
     </div>
 
     <!-- <BattleShipHeader/>
@@ -64,49 +78,67 @@ export default {
       shipLength: 2,
       shipDirection: '0',
       coordinate: null,
-      latitude: 0,
-      longtitude: 0
+      selectedX: 0,
+      selectedY: 0,
+      errorMessage: null
     }
   },
   computed: {
+    validateShipLength: function () {
+      if ((this.shipLength < 2) || (this.shipLength > 5)) {
+        console.log(this.shipLength)
+        console.log('ship length should be between 2 to 5')
+        return false
+      }
+      return true
+    },
     getSelectedCoordinates: function () {
       let selectedCoordinates = []
-      // valiate ship length
-      if ((this.shipLength < 2) || (this.shipLength > 5)) {
-        console.log('ship length should be between 2 to 5')
-        return selectedCoordinates
-      }
       for (let i = 0; i < this.shipLength; ++i) {
-        let lat = this.latitude
-        let lng = this.longtitude
+        let selectedX = Number(this.selectedX)
+        let selectedY = Number(this.selectedY)
         if (Number(this.shipDirection) === 0) {
           // update horizontal point
-          lng = lng + i
+          selectedY = selectedY + i
         } else {
           // update vertical point
-          lat = lat + i
+          selectedX = selectedX + i
         }
-        // validate point
-        if (this.validatePt(lat, lng)) {
-          // push to selected array
-          selectedCoordinates.push({ lat, lng })
-        }
+        selectedCoordinates.push({ selectedX, selectedY })
       }
-      console.log(selectedCoordinates)
       return selectedCoordinates
     }
   },
   methods: {
-    validatePt: function (lat, lng) {
-      console.log(lat, lng)
-      if (lat >= this.boardSize || lng >= this.boardSize) {
-        console.log('Oops, you are trying to set a ship out of the board')
+    validateOutOfBox: function (coordinate) {
+      if (coordinate.selectedX >= this.boardSize || coordinate.selectedY >= this.boardSize) {
+        return true
+      }
+      return false
+    },
+    validateIfTaken: function (coordinate) {
+      if (Number(this.board[coordinate.selectedX][coordinate.selectedY]) !== 0) {
+        return true
+      }
+      return false
+    },
+    validatePts: function (coordinates) {
+      if (!this.validateShipLength) {
+        this.errorMessage = 'Ship length should be between 2 to 5'
         return false
       }
-      if (Number(this.board[lat][lng]) !== 0) {
-        console.log('Oops, one of the points are already taken')
-        return false
+      for (var coordinate of coordinates) {
+        if (this.validateOutOfBox(coordinate)) {
+          this.errorMessage = 'Oops, you are trying to set a ship out of the board'
+          return false
+        }
+        if (this.validateIfTaken(coordinate)) {
+          this.errorMessage = 'Oops, one of the points are already taken'
+          return false
+        }
       }
+
+      this.errorMessage = null
       return true
     },
     selectedCoordinate: function (index, type) {
@@ -126,14 +158,20 @@ export default {
     setCoordinate (index, type) {
       this[type] = index
     },
-    setShipOnBoard () {
+    setShipOnBoard (elem) {
+      // validate if board enabled
+      if (!this.boardSet) return
+      // get selected coordinates
+      this.selectedX = elem.currentTarget.getAttribute('x')
+      this.selectedY = elem.currentTarget.getAttribute('y')
       let coordinates = this.getSelectedCoordinates
       console.log(coordinates)
-      // set coordinates
-      coordinates.forEach((coordinate) => {
-        this.board[coordinate.lat][coordinate.lng] = 1
-      })
-      console.log(this.board)
+      console.log(this.validatePts(coordinates))
+      if (this.validatePts(coordinates)) {
+        coordinates.forEach((coordinate) => {
+          this.board[coordinate.selectedX][coordinate.selectedY] = 1
+        })
+      }
     }
   }
   // components: {
@@ -145,29 +183,29 @@ export default {
 </script>
 <style scoped>
 .game {
-  display: flex;
-  flex-direction: column;
+  display: grid;
+  grid-template-rows: 20vh 10vh auto 5vh;
 }
-
+.board{
+      display: flex;
+    justify-content: center;
+}
 .game .actions {
   height: 20vh;
 }
 .game .actions button {
   padding: 2rem;
 }
-.game button.boardSet {
-  /* background: gray;
-  color: lightgreen;
-  font-weight: bold; */
-}
 
-.game .setShip {
+.game .shipConfig {
   display: flex;
-  flex-direction: column;
-  justify-content: space-around;
+  /* flex-direction: column;
+  justify-content: space-around; */
   text-align: left;
   width:96vw;
   margin: 0 auto;
+  flex-direction: row;
+  justify-content: space-evenly;
 }
 .active {
   color: #fff;
@@ -204,5 +242,23 @@ li {
   /* background-color: green;
   color: aliceblue */
 }
-
+td {
+  width: 50px;
+  height:50px;
+  background: gray;
+}
+td.activeShips{
+  background: #438e43;
+}
+td.activeShips:hover{
+  background: lightskyblue;
+  cursor: pointer;
+}
+td.activeShips.taken{
+  background: lightcoral;
+}
+.error-message {
+  color: red;
+  font-weight: bold;
+}
 </style>
